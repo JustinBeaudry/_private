7e3ed241320877e513c52a64646d596bc03276cb
JustinBeaudry <justin.beaudry+github@creativelive.com>
2014-12-10 01:42:22 -0800

    adds window.openIncognito and the basic form for settings

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-private-session
-===============
+# [![_private](https://raw.githubusercontent.com/JustinBeaudry/_private/chrome/assets/_private_64.png)](https://github.com/JustinBeaudry/_private) _private
 
-The purpose of this extension is to create a multi-platform method of allowing an HTML '\&lt;a\>' to use target='\_private' to launch a private browsing session.
+A multi-platform method of allowing an HTML &lt;a&gt; to use target='\_private' to launch a private browsing session.
+
+##Chrome Extension
+
+In Progress
+
+##Firefox Addon
+
+Coming Soon
+
+##Safari Extension
+
+Coming Soon
+
+---
+
+Private Session logo designed by [Dan Hetteix](http://thenounproject.com/DHETTEIX) from the [Noun Project](http://thenounproject.com/) :: Creative Commons – Attribution (CC BY 3.0)
+
---
+++ _private.js
@@ -0,0 +1,70 @@
+(function(window, document){
+  'use strict';
+  
+  var NAME = '[_private]';
+  var options = chrome.storage.local.get('_private:settings') || {};
+
+  function open_incognito(opts) {
+    if (!opts) {
+      throw new Error(NAME + ' open_incognito requires an options object');
+    }
+
+    var url = opts.url || {};
+    var type = opts.type || "popup";
+    var focus = opts.focus || true;
+
+    if (url === undefined || url === null || url === Object || url === Boolean || url === Function) {
+      throw new Error('window.openIncognito requires a string or an array of urls');
+    }
+    if (options.verbose) {
+      console.info(NAME + ' Opening Incognito Window');
+    }
+
+    chrome.windows.create({
+      url: url,
+      focus: focus,
+      type: type,
+      incognito: true
+    });
+  }
+
+  if (!window.openIncognito) {
+    window.openIncognito = function open_incognito_window(url, type, focus) {
+
+      var opts = {
+        url: url,
+        type: type,
+        focus: focus
+      };
+
+      open_incognito(opts);
+    };
+  }
+
+  var a_elements = document.body.getElementsByTagName('a');
+
+  for (var i = 0; i < a_elements.length; i += 1) {
+
+    var element = a_elements[i];
+    var target = element.attributes.getNamedItem('target');
+    var href = element.attributes.getNamedItem('href');
+
+    if (target && target._private) {
+
+      target.addEventListener('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        window.openIncognito(href, (options.type || null), (options.focus || null));
+
+      }, false);
+    }
+  }
+
+  var settings = {
+    type: document.getElmentById('type'),
+    focus: document.getElmentById('focus'),
+    verbose: document.getElmentById('verbose')
+  };
+
+}(window, document));
